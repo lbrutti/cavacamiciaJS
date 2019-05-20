2befb85c88f25c2d52ef337272ef4b68f3b1ab6d
ekr - lorenzo.brutti <lorenzo.brutti@ekr.it>
2019-05-20 16:30:40 +0200

    fix logica

--- stracciaJS.js
+++ stracciaJS.js
@@ -28,29 +28,47 @@ function shuffle(array) {
 
   // While there are elements in the array
   while (counter > 0) {
-      // Pick a random index
-      let index = Math.floor(Math.random() * counter);
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
 
-      // Decrease counter by 1
-      counter--;
+    // Decrease counter by 1
+    counter--;
 
-      // And swap the last element with it
-      let temp = array[counter];
-      array[counter] = array[index];
-      array[index] = temp;
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
   }
 
   return array;
 }
 
-function play_hand(playing_deck, opponent_deck, table_deck, turn, remaining_payment, deck1_start, deck2_start) {
+/**
+ * Simula una partita di straccia camicia secondo queste regole:
+ * 
+ * il giocatore corrente mette la prima carta del suo mazzo sul tavolo
+ * il turno passa al giocatore successivo
+ * il successivo mette la sua carta
+ * 
+ * 
+ * @param {array} playing_deck : mazzo del primo giocatore
+ * @param {array} opponent_deck : mazzo del secondo giocatore
+ * @param {array} table_deck : mazzo in tavola
+ * @param {int} turn : turno di gioco
+ * @param {int} remaining_payment : carte da scartare
+ * @param {array} playing_deck_start : configurazione iniziale del mazzo del primo giocatore
+ * @param {array} opponent_deck_start :  configurazione iniziale del mazzo del secondo giocatore
+ */
+function play_hand(playing_deck, opponent_deck, table_deck, turn, remaining_payment, playing_deck_start, opponent_deck_start) {
   // stop se il giocatore di turno ha finito il mazzo
   if ((playing_deck.length == 0 || opponent_deck.length == 0)) {
+    console.log(playing_deck.toString());
+    console.log(opponent_deck.toString());
     console.log("fine!", turn);
     return turn;
   }
   //stop se sono in loop
-  if (turn > 1 && ((playing_deck.toString() == deck1_start.toString()) && (opponent_deck.toString() == deck2_start.toString()))) {
+  if (turn > 1 && ((playing_deck.toString() == playing_deck_start.toString()) && (opponent_deck.toString() == opponent_deck_start.toString()))) {
 
     console.log("LLLOOOOOOPPP");
     console.log(playing_deck.toString());
@@ -60,21 +78,25 @@ function play_hand(playing_deck, opponent_deck, table_deck, turn, remaining_paym
     return "loop!";
   }
   //altrimenti metti sul tavolo la prima carta
-  let last_element = playing_deck.shift();
+  let last_element = Number(playing_deck.shift());
   //aggiungo in testa
   table_deck.unshift(last_element);
   switch (last_element) {
     case 1:
     case 2:
     case 3:
-      return play_hand(opponent_deck, playing_deck, table_deck, ++turn, last_element, deck2_start, deck1_start);
+      console.log("Giocata carta potente : ", last_element);
+      console.log(playing_deck.toString());
+      console.log(opponent_deck.toString());
+      return play_hand(opponent_deck, playing_deck, table_deck, ++turn, last_element, opponent_deck_start, playing_deck_start);
     default:
+      console.log("Giocata carta neutra : ", last_element);
       if (remaining_payment == 0) {
         opponent_deck = opponent_deck.concat(table_deck);
         table_deck = [];
-        return play_hand(opponent_deck, playing_deck, table_deck, ++turn, 1, deck2_start, deck1_start);
+        return play_hand(opponent_deck, playing_deck, table_deck, ++turn, 1, opponent_deck_start, playing_deck_start);
       } else {
-        return play_hand(playing_deck, opponent_deck, table_deck, turn, --remaining_payment, deck1_start, deck2_start);
+        return play_hand(playing_deck, opponent_deck, table_deck, turn, --remaining_payment, playing_deck_start, opponent_deck_start);
       }
 
   }
@@ -83,26 +105,14 @@ function play_hand(playing_deck, opponent_deck, table_deck, turn, remaining_paym
 
 // infinita : 0030202030310000031000100200002000000010
 
-//let deckString =  "0030202030310000031000100200002000000010";
-let deckString =    "111122223333";
+let deckString = "0030202030310000031000100200002000000010";
 let deck = deckString.split('');
-// deck = "003020".split('');
-function playAllPermutations(deck) {
-  let permutations = permute(deck);
-  permutations.forEach(permutation => {
-    let deck1 = permutation.slice(0, 20);
-    let deck2 = permutation.slice(20, 40);
-    let deck1_start = permutation.slice(0, 20);
-    let deck2_start = permutation.slice(20, 40);
-    play_hand(deck1, deck2, [], 0, 1, deck1_start, deck2_start);
-  });
-}
 
-for(let i =0; i<100; i++){
-  let permutation = (deckString.split(''));
-  let deck1 = permutation.slice(0, 20);
-  let deck2 = permutation.slice(20, 40);
-  let deck1_start = permutation.slice(0, 20);
-  let deck2_start = permutation.slice(20, 40);
-  play_hand(deck1, deck2, [], 0, 1, deck1_start, deck2_start);
-}
+let deck1 = deck.slice(0, 20);
+let deck2 = deck.slice(20, 40);
+let deck1_start = deck.slice(0, 20);
+let deck2_start = deck.slice(20, 40);
+console.log("mazzo 1: ", deck1.toString());
+console.log("mazzo 2: ", deck2.toString());
+play_hand(deck1, deck2, [], 0, 1, deck1_start, deck2_start);
+
