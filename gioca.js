1b8aa57efffe5201dcd2f523fa93e44abeeb4b69
Lorenzo Brutti <lorenzobrutti80@gmail.com>
2019-05-21 23:48:56 +0200

    rename

--- gioca.js
+++ gioca.js
@@ -1,41 +1,41 @@
-function gioca(iniziatore = [], sfidante = [], banco = [], turno = 0, residuo = 1) {
+function gioca(p1 = [], p2 = [], banco = [], turno = 0, residuo = 1) {
     //caso base: mi fermo quando il giocatore in turno non ha piu' carte
-    if (iniziatore.length == 0) {
-        sfidante = sfidante.concat(banco);
-        console.log("iniziatore = ", iniziatore.toString());
-        console.log("sfidante = ", sfidante.toString());
+    if (p1.length == 0) {
+        p2 = p2.concat(banco);
+        console.log("p1 = ", p1.toString());
+        console.log("p2 = ", p2.toString());
 
         return "fine";
     }
     //passo: faccio le giocate
     if (residuo == 0) {
-        sfidante = sfidante.concat(banco);
-        console.log("lo sfidante prende il banco :", banco.toString());
+        p2 = p2.concat(banco);
+        console.log("lo p2 prende il banco :", banco.toString());
 
-        return gioca(sfidante, iniziatore, [], turno++);
+        return gioca(p2, p1, [], turno++);
     }
-    //iniziatore estrae prima carta dal mazzo e la mette nel banco
-    let carta = iniziatore.shift();
+    //p1 estrae prima carta dal mazzo e la mette nel banco
+    let carta = p1.shift();
     banco.push(carta); //appendo in coda in modo che banco sia uno stack FIFO
-    //se iniziatore ha appena giocato una carta potente 
-    //il turno passa a sfidante che deve scartare "carta" carte
+    //se p1 ha appena giocato una carta potente 
+    //il turno passa a p2 che deve scartare "carta" carte
     if (carta == 1 || carta == 2 || carta == 3) {
-        return gioca(sfidante, iniziatore, banco, turno, carta);
+        return gioca(p2, p1, banco, turno, carta);
     }
     //se ho estratto una carta neutra
     else {
-        //se non devo piu' estrarre carte -> sfidante fa la presa e gioca
+        //se non devo piu' estrarre carte -> p2 fa la presa e gioca
         if (residuo == 0) {
             console.log("qui non arriva mai");
 
-            //sfidante ruba il mazzo dal banco
-            sfidante = sfidante.concat(banco);
-            // iniziatore inizia nuova giocata
-            return gioca(sfidante, iniziatore, [], turno++, 1);
+            //p2 ruba il mazzo dal banco
+            p2 = p2.concat(banco);
+            // p1 inizia nuova giocata
+            return gioca(p2, p1, [], turno++, 1);
         } else {
-            //se devo ancora buttare carte passa la mano a sfidante
+            //se devo ancora buttare carte passa la mano a p2
             //decrementando il residuo
-            return gioca(iniziatore, sfidante, banco, turno, --residuo);
+            return gioca(p1, p2, banco, turno, --residuo);
         }
     }
 
